e4ce797bb62bb58b5645ca5b41e53e1a58c2fcca
suraj <suraj@ip-192-168-1-100.ap-south-1.compute.internal>
2025-01-03 16:07:26 +0530

    implemented clearAllTimeout

---
+++ src/javascript/problem3.js
@@ -0,0 +1,31 @@
+/**
+ * Implement clearAllTimeout in js 
+ * 
+ * 
+ */
+
+
+(function (w){
+    let timeoutIds = [];
+    let origininalSetTimeout = w.setTimeout;
+    w.setTimeout = function(...args){
+     let id = origininalSetTimeout(...args);
+     timeoutIds.push(id)
+     return id
+    }
+    w.clearAllTimeout =  function(){
+     while(timeoutIds.length){
+       console.log('clearing IDS')
+       clearTimeout(timeoutIds.pop());
+     }
+    }
+ })(global)
+ 
+ 
+ 
+ setTimeout(()=>console.log('One'),2000)
+ setTimeout(()=>console.log('Two'),3000)
+ setTimeout(()=>console.log('Three'),4000)
+ setTimeout(()=>console.log('Four'),4000)
+ 
+ clearAllTimeout()
