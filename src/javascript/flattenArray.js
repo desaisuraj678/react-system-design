dc530b1634d12725a7f8dc3592eae7be83a34156
suraj <suraj@Shubhams-MacBook-Pro.local>
2025-01-07 10:27:53 +0530

    flatten array changes

--- src/javascript/flattenArray.js
+++ src/javascript/flattenArray.js
@@ -4,19 +4,25 @@
  * 
  */
 
-let output = []
 
-function flattenArray(arr){
-    for(let i=0;i<arr.length;i++){
-        let item = arr[i];
-        if(Array.isArray(item)){
-            flattenArray(item)
-        } else{
-            output.push(item)
+function flattenArray(){
+    let output = []
+    function process(arr){
+        for(let i=0;i<arr.length;i++){
+            let item = arr[i];
+            if(Array.isArray(item)){
+                process(item)
+            } else{
+                output.push(item)
+            }
         }
     }
+    process(this)
+    return output
 }
-const arr = [1,2,[3,4],[5,[6]]]
-flattenArray(arr)
 
-console.log(output)
+Array.prototype.flatten = flattenArray
+const arr = [1,2,[3,4],[5,[6]]]
+const ans = arr.flatten()
+
+console.log(ans)
